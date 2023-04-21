12eaa0b8a56aa2e0739a6dfb9959eefe7a507d9d
Reed Es <reedes@fastmail.com>
2023-04-20 20:15:49 -0600

    Update to oldest supported versions

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 
 let package = Package(
     name: "FINporterFido",
-    platforms: [.macOS(.v10_12)],
+    platforms: [.macOS(.v10_13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
